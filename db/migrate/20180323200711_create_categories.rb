7c99c8e929c960c0fda8aa475419e88866f6758f
kylesallette <kylesallette@gmail.com>
2018-03-24 15:15:12 -0600

    adding new id

--- db/migrate/20180323200711_create_categories.rb
+++ db/migrate/20180323200711_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
       t.string :name
-      t.references :job, foreign_key: true
+    
 
       t.timestamps
     end
