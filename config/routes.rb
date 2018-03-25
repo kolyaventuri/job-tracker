1a5ba53821044d63d0ec60ad389065f744dcf01b
kylesallette <kylesallette@gmail.com>
2018-03-25 13:37:58 -0600

    welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 end
      resources :comments
 
+root 'welcome#index' 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
