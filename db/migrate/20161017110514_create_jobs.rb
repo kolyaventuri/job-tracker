91af780ef536a652c351872b6d04abf746a55f44
kylesallette <kylesallette@gmail.com>
2018-03-26 10:26:56 -0600

    fixed welcome page

--- db/migrate/20161017110514_create_jobs.rb
+++ db/migrate/20161017110514_create_jobs.rb
@@ -4,7 +4,7 @@ class CreateJobs < ActiveRecord::Migration[5.1]
       t.string :title
       t.string :description
       t.integer :level_of_interest
-
+      
       t.timestamps null: false
     end
   end
