36a58a466b0a523443e518063cba66d233e27faa
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 23:55:39 -0600

    Create contact test

---
+++ spec/features/contacts/user_can_create_a_contact_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'User' do
+  before(:all) do
+    DatabaseCleaner.clean
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  describe 'visiting a company' do
+    it 'should be able to add a contact' do
+      company = Company.create!(name: 'ESPN')
+      name = 'Bob Ross'
+      role = 'Painter'
+      email = 'Bob@BobRoss.com'
+
+      visit company_path(company)
+
+      fill_in 'contact[name]', with: name
+      fill_in 'contact[role]', with: role
+      fill_in 'contact[role]', with: email
+
+      click_on 'Create Contact'
+
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content(name)
+      expect(page).to have_content(role)
+    end
+  end
+end
