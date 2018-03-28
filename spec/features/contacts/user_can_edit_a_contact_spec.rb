bc1b47dfca519b47f538909c52ca7cb9c6a405d7
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-28 00:08:39 -0600

    Edit contact test

---
+++ spec/features/contacts/user_can_edit_a_contact_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'User' do
+  before(:all) do
+    DatabaseCleaner.clean
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  describe 'visiting a company page' do
+    it 'should be able to edit a contact' do
+      company  = Company.create!(name: 'ESPN')
+      contact1 = Contact.create!(name: 'Bob', email: 'bob@bob', role: 'PM', company: company)
+      contact2 = Contact.create!(name: 'Sally', email: 'sally@sally', role: 'MG', company: company)
+      new_name = 'Sampson'
+      
+      visit company_path(Company.last)
+
+      expect(page).to have_content contact1.name
+      expect(page).to have_content contact2.name
+
+      find("#edit_#{contact1.id}").click
+
+      expect(current_path).to eq(edit_company_contact_path(comapny, contact1))
+
+      fill_in 'contact[name]', with: new_name
+      click_on 'Update Contact'
+
+      expect(current_path).to eq(company_path(Company.last))
+
+      expect(page).to have_content new_name
+      expect(page).to have_content contact2.name
+      expect(page).to_not have_content contact1.name
+    end
+  end
+end
